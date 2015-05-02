05d6e81cb5c8abc87158b61e1abe7083d56d63b8
Alexander Lozovskoy <alozovskoy@gmail.com>
2015-05-02 05:08:29 +0300

    Путь к инвентарю и сценарию сохраняется в виде полного пути от рабочей деректории Ansible

--- callbacks/taskinfo.py
+++ callbacks/taskinfo.py
@@ -4,17 +4,33 @@ import os
 import sqlite3
 
 task_id = os.environ['ANSIBLE_TASK_ID']
+ansible_home = os.environ['ANSIBLE_HOME']
+pwd = os.getcwd()
 
+def getPath(path):
+	_path = os.path.join(pwd, path)
+	filePath = path
+	if os.path.exists(_path) and os.path.isfile(_path):
+		filePath = _path
+	else:
+		if os.path.exists(path) and os.path.isfile(path):
+			filePath = path
+
+	filePath = filePath.replace(ansible_home, '', 1)
+	return	filePath
+	
 
 class CallbackModule(object):
 
     def playbook_on_play_start(self, pattern):
+	playbookPath = getPath(self.playbook.filename)
+	inventoryPath = getPath(self.playbook.inventory.src())
         con = sqlite3.connect('/home/ansible/ansible.db', 10.0)
         cur = con.cursor()
         cur.execute('update tasks set ppid = ?, playbook = ?, inventory = ?, vars = ?, tags = ?, skipped_tags = ? where id = ?', (
             os.getpid(),
-            self.playbook.filename,
-            self.playbook.inventory.src(),
+            playbookPath,
+            inventoryPath,
             str(self.playbook.extra_vars),
             ', '.join(self.playbook.only_tags),
             ', '.join(self.playbook.skip_tags),
